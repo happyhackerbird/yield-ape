e721172587922732a7bf5d83043a4afd1079215b
Lilly <reubenh14@tutanota.com>
2023-09-24 00:06:00 -0400

    contracts mainnet

--- contracts/src/Ape.sol
+++ contracts/src/Ape.sol
@@ -1,20 +1,26 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 import "src/interfaces/IAxelarGateway.sol";
+import "src/interfaces/IERC20.sol";
 
 contract Ape {
     address constant AXELAR_MANTLE_GATEWAY =
-        0xe432150cce91c13a887f7D836923d5597adD8E31; //TESTNET
+        0xe432150cce91c13a887f7D836923d5597adD8E31; //mainnet
 
-    function ape() external {
+    address constant axlUSDC = 0xEB466342C4d449BC9f53A865D5Cb90586f405215;
+
+    function ape(
+        bytes memory payload,
+        uint amount,
+        address yieldToken
+    ) external {
+        IERC20(axlUSDC).approve(AXELAR_MANTLE_GATEWAY, amount);
         IAxelarGateway(AXELAR_MANTLE_GATEWAY).callContractWithToken(
-            "base",
-            "0xA233441c94b2e13eaA9147849c1ed7e774C03047", // destination - yield contract on base
-            abi.encodePacked(
-                address(0xcA85486e554c15f0721C815520b10d0874669572)
-            ), //payload
-            "mnt",
-            1
+            "base", //destination chain
+            "0xA233441c94b2e13eaA9147849c1ed7e774C03047", // destination contract - yield contract on base
+            payload,
+            "uusdc",
+            amount
         );
     }
 }
--- contracts/src/Yield.sol
+++ contracts/src/Yield.sol
@@ -5,7 +5,11 @@ import "src/interfaces/IPool.sol";
 import "src/interfaces/IERC20.sol";
 
 contract Yield is AxelarExecutable {
-    // address constant AAVE_POOL = 0xA238Dd80C259a72e81d7e4664a9801593F98d1c5; //mainnet
+    address constant AAVE_POOL = 0xA238Dd80C259a72e81d7e4664a9801593F98d1c5; //mainnet
+    address constant ONEINCH_ROUTER =
+        0x1111111254EEB25477B68fb85Ed929f73A960582;
+    address constant USDbC = 0xd9aAEc86B65D86f6A7B5B1b0c42FFA531710b6CA;
+    address constant axlUSDC = 0xEB466342C4d449BC9f53A865D5Cb90586f405215;
 
     constructor(address gateway_) AxelarExecutable(gateway_) {}
 
@@ -16,16 +20,31 @@ contract Yield is AxelarExecutable {
         string calldata tokenSymbol,
         uint256 amount
     ) internal override {
-        // decode recipient
-        // address recipient = address(this);
-        address recipient = abi.decode(payload, (address));
+        address recipient = address(this);
         // get ERC-20 address from gateway
         address tokenAddress = gateway.tokenAddresses(tokenSymbol);
 
+        // transfer tokens from gateway to this contract
+        IERC20(tokenAddress).transfer(recipient, amount);
+
+        swapWith1Inch(payload);
+
+        // decode payload
+        // address _payload = abi.decode(payload, (address));
+        // swap using calldata
+
         // deposit into Aave
-        // IPool(AAVE_POOL).deposit(tokenAddress, amount, recipient, 0);
+        IPool(AAVE_POOL).deposit(tokenAddress, amount, recipient, 0);
 
         // transfer received tokens to the recipient
-        IERC20(tokenAddress).transfer(recipient, amount);
+    }
+
+    function swapWith1Inch(bytes calldata swapCallData) public {
+        uint balance = IERC20(axlUSDC).balanceOf(address(this));
+        IERC20(axlUSDC).approve(ONEINCH_ROUTER, balance);
+        (bool success, bytes memory retData) = ONEINCH_ROUTER.call(
+            swapCallData
+        );
+        require(success, "swap failed");
     }
 }
